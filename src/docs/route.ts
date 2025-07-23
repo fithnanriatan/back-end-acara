0750048ec62d9b03d5767e0491d52edf69da2b62
Fithnan Ri'atan <natairnanhtif@gmail.com>
2025-07-23 14:04:30 +0700

    bug fix

--- src/docs/route.ts
+++ src/docs/route.ts
@@ -1,21 +1,15 @@
 import { Express } from "express"
 import swaggerUi from "swagger-ui-express"
 import swaggerDocument from "./swagger_output.json"
-import fs from "fs"
-import path from "path"
 
 export default function docs(app: Express) {
-
-    const css = fs.readFileSync(path.resolve(
-        __dirname,
-        "../../node_modules/swagger-ui-dist/swagger-ui.css"
-    ), "utf-8")
 
     app.use(
         "/api-docs",
         swaggerUi.serve,
         swaggerUi.setup(swaggerDocument, {
-            customCss: css
+            customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui.min.css"
         })
-    )
+    );
+
 }
