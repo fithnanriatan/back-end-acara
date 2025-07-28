d0e80e390cd004be17a7ed1c691f6388b7239b56
Fithnan Ri'atan <natairnanhtif@gmail.com>
2025-07-28 20:05:55 +0700

    tes2

---
+++ src/docs/swagger.ts
@@ -0,0 +1,38 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+    info: {
+        version: "v0.0.1",
+        title: "Dokumentasi API ACARA",
+        description: "Dokumentasi API ACARA"
+    },
+    servers: [
+        {
+            url: "http://localhost:3000/api",
+            description: "Local Server"
+        },
+        {
+            url: "https://back-end-acara-fithnan.vercel.app/api",
+            description: "Deploy Server"
+        },
+    ],
+    components: {
+        securitySchemes: {
+            bearerAuth: {
+                type: "http",
+                scheme: "bearer",
+            }
+        },
+        schemas: {
+            LoginRequest: {
+                identifier: "natairnanhtif",
+                password: "nata739"
+            }
+        }
+    }
+}
+
+const outputFile = "./swagger_output.json";
+const endpointsFiles = ["../routes/api.ts"]
+
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, doc);
